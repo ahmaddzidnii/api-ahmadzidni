5e7a3e0b33143d37db0efcbf9b7a030ab1b1b2de
ahmaddzidnii <zdngaming312@gmail.com>
2024-02-13 11:57:32 +0700

    test: test deploy vercel

--- src/controllers/root/getRoot.js
+++ src/controllers/root/getRoot.js
@@ -3,12 +3,16 @@
  * @param {import('express').Request} _req
  * @param {import('express').Response} res
  */
-const getRoot = async (_req, res) => {
-  const respone = await fetch("https://api.myquran.com/v2/sholat/kota/semua");
-  const data = await respone.json();
-  res.status(200).json({
-    data,
-  });
+const getRoot = async (_req, res, next) => {
+  try {
+    const respone = await fetch("https://api.myquran.com/v2/sholat/kota/semua");
+    const data = await respone.json();
+    throw new Error("test error");
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
-
 export default getRoot;
